6ded4e1fa729dec085d6e05f0a4d46feb9baa8d4
williamzhu17 <williammzhu@gmail.com>
2025-11-19 23:37:41 -0800

    renamed corpus and updated README

--- tests/test_medium.cpp
+++ tests/test_medium.cpp
@@ -271,7 +271,7 @@ int main() {
     std::cout << "All forward and recovered files will be saved for inspection.\n" << std::endl;
     
     // Define the data directory and block sizes to test
-    std::string data_dir = "../data/medium_size";
+    std::string data_dir = "../data/canterbury_corpus";
     // std::vector<size_t> block_sizes = {128, 256, 512, 1024, 4096, 16384};
     std::vector<size_t> block_sizes = {128};
     
